72c0f0f22c6d5f6e0379ad5bccc248f333051aa6
Jobert Gutierrez <jobert.gutierrez@gmail.com>
2024-02-23 04:42:30 +0000

    fact_fhv merged with yellow,green,fhv

--- taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -10,32 +10,39 @@ with fhv_tripsdata as
     from {{ ref('stg_fhv_tripdata') }}
 ),
 green_tripdata as (
-    select *, 
+    select *,  
         'Green' as service_type
     from {{ ref('stg_green_tripdata') }}
 ), 
 yellow_tripdata as (
-    select *, 
+    select *,
         'Yellow' as service_type
     from {{ ref('stg_yellow_tripdata') }}
+), 
+trips_unioned as (
+    select tripid, pickup_locationid, dropoff_locationid, pickup_datetime, dropoff_datetime,service_type, from green_tripdata
+    union all 
+    select tripid, pickup_locationid, dropoff_locationid, pickup_datetime, dropoff_datetime,service_type, from yellow_tripdata
+    union all 
+    select tripid, pickup_locationid, dropoff_locationid, pickup_datetime, dropoff_datetime,service_type, from fhv_tripsdata
 ), 
 dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
 )
-select fhv_tripsdata.tripid,
-    fhv_tripsdata.pickup_locationid,
+select trips_unioned.tripid,
+    trips_unioned.pickup_locationid,
     pickup_zone.borough as pickup_borough, 
     pickup_zone.zone as pickup_zone, 
-    fhv_tripsdata.dropoff_locationid,
+    trips_unioned.dropoff_locationid,
     dropoff_zone.borough as dropoff_borough, 
     dropoff_zone.zone as dropoff_zone,  
-    fhv_tripsdata.pickup_datetime,
-    fhv_tripsdata.dropoff_datetime,
-    fhv_tripsdata.SR_flag,
-    fhv_tripsdata.service_type
-from fhv_tripsdata
+    trips_unioned.pickup_datetime,
+    trips_unioned.dropoff_datetime,
+    -- trips_unioned.SR_flag,
+    trips_unioned.service_type
+from trips_unioned
 inner join dim_zones as pickup_zone
-on fhv_tripsdata.pickup_locationid = pickup_zone.locationid
+on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
-on fhv_tripsdata.dropoff_locationid = dropoff_zone.locationid
+on trips_unioned.dropoff_locationid = dropoff_zone.locationid
