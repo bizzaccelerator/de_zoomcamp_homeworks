d604daf12320787a092e388e84c6fd1c04864002
Jobert Gutierrez <jobert.gutierrez@gmail.com>
2024-02-23 00:41:28 +0000

    fhv model adjusted and prepared table created.

--- taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -15,10 +15,15 @@ dim_zones as (
 )
 select fhv_tripsdata.tripid,
     fhv_tripsdata.pickup_locationid,
+    pickup_zone.borough as pickup_borough, 
+    pickup_zone.zone as pickup_zone, 
     fhv_tripsdata.dropoff_locationid,
+    dropoff_zone.borough as dropoff_borough, 
+    dropoff_zone.zone as dropoff_zone,  
     fhv_tripsdata.pickup_datetime,
     fhv_tripsdata.dropoff_datetime,
-    fhv_tripsdata.SR_flag
+    fhv_tripsdata.SR_flag,
+    fhv_tripsdata.service_type
 from fhv_tripsdata
 inner join dim_zones as pickup_zone
 on fhv_tripsdata.pickup_locationid = pickup_zone.locationid
